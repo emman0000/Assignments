b65f2269e7dce37d2b843df2739bf4dad20b930a
emman0000 <142867499+emman0000@users.noreply.github.com>
2023-09-12 10:56:43 +0500

    Add files via upload

---
+++ LAB_03/task2.c
@@ -0,0 +1,21 @@
+	// Programmar: Emman Abrar Ali
+// 12/09/2023
+// task 2 n number of rows of stars *
+#include<stdio.h>
+main(){
+int i,n,j;
+printf("Enter the number of rows you would like=");
+scanf("%d",&n);
+
+
+i=1;
+while (i<=n){
+j=1;} //end while i<=n
+while (j<=n){
+printf("*");
+j++;
+}//end while j<=n
+printf("\n");
+i++;
+}
+}
